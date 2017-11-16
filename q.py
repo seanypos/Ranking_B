62379ab250f88602f233e898a744d0d12ae7821a
Reilly Keating-Wood <reitoast156@gmail.com>
2017-11-15 20:26:33 -0500

    q is practice for flask, and qt is test JSON data

---
+++ q.py
@@ -0,0 +1,7 @@
+"""
+Querying:
+1. Receive queries and send sorted urls back
+2. Receive clicks for urls
+"""
+
+#practice flask work things in here
---
+++ qt.py
@@ -0,0 +1,7 @@
+
+
+"""
+Fake Queries here. Ideally this generally what query will be sending to us 'hopefully'
+"""
+
+{ "query":"healthy","food"}
