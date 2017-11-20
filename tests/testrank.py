6f96e82c1c781fa3cae7cfed5ea44119b1079e0a
aleung013 <aleung013@gmail.com>
2017-11-20 14:05:33 -0500

    Add more tests for indexing

--- tests/testrank.py
+++ tests/testrank.py
@@ -22,5 +22,12 @@ class TestRank(unittest.TestCase):
         result = rank(['uri'])
         self.assertEqual(result, ['uri'])
 
+    def test_rank_sorted(self):
+        '''
+        Test that the ranking of a sorted list returns the correct result
+        '''
+        result = rank(['Hello', 'World'])
+        self.assertEqual(result, ['Hello', 'World'])
+
 if __name__ == '__main__':
     unittest.main()
