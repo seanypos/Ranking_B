841e1814062a517a979e246457e6c7520b458e2c
Sean Postler <postls@rpi.edu>
2017-11-27 10:42:52 -0500

    added linkanalysis weights to the dictionary

--- reilly.py
+++ reilly.py
@@ -24,14 +24,59 @@ def test_connection(self):
 def getPageRank():
      data = json.load(open('link_analysis.json'))
      return data
+ 
+def getIndexing():
+    data = json.load(open('indexing.json'))
+    return data
+
+def getQuery():
+    return
+
+
+
+def checkweight(urls, weights):
+    for item in urls:
+        print(weights[item])
+'''
+Iterate through indexing Json and create dictionary with all url keys
+'''
+def create_dict(indexing):
+    weights = {}
+    #print(indexing["documents"][0]["documentID"])
+    all_urls = []
+    for x in range(len(indexing["documents"])):
+        weights[indexing["documents"][0]["documentID"]] = 0
+        all_urls.append(indexing["documents"][x]["documentID"])
+    return weights, all_urls
+
+'''
+Function adds weight to the dictionary of urls for each url
+'''
+def add_weight(indexing, link_analysis, urls, weights):
+    ## Add link_analysis weight will update multiplayer when testing
+    print(len(link_analysis["test"]))
+    for item in link_analysis["test"]:
+        add = item["pageRankValue"] * .2
+        print(add)
+        weights[item["webpage"]] = add
+        
+    ## Add indexing weight multiplier
     
-
-
+    
+    checkweight(urls,weights)
 if __name__ == "__main__":
     #test_connection(app)
-    print("test")
     #app.run(debug=True)
     #data = getPageRank()
-    data = getPageRank()
+    link_analysis = getPageRank()
+    indexing = getIndexing()
+    weights, urls = create_dict(indexing)
+    weights = add_weight(indexing, link_analysis, urls, weights)
+    
+    
+    
+    
+    
+    
     #pprint(data)
-    pprint(data['test'])
+    #pprint(indexing)
