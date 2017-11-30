3372be733304f59c7819e2d5f6c6b2eb287cab54
Reilly Keating-Wood <reitoast156@gmail.com>
2017-11-29 20:09:57 -0500

    query updates

--- reilly.py
+++ reilly.py
@@ -1,8 +1,8 @@
 """
-Hello my Name Is reilly Keating-Wood and 
-for ranking team B i am CFO. The 
-Chief Financial Officer
-"""
+    Hello my Name Is reilly Keating-Wood and
+    for ranking team B i am CFO. The
+    Chief Financial Officer
+    """
 
 #from flask import Flask
 #from flask import json
@@ -11,46 +11,26 @@ from pprint import pprint
 #from flask import jsonify
 #app = Flask(__name__)
 
-<<<<<<< HEAD
-#return like so ~~
-"""
-    {
-    "ranking": [
-    {
-    "url": "www.url.com",
-    "rank": 1,
-    "positions": {"query": [1, 55, 3000], "test": [5], "example": [2, 90], ... }
-    },
-    {
-    "url": "www.secondurl.com",
-    "rank": 2,
-    "positions": {"query": [34], "test": [34, 78, 989, 234325], ... }
-    },
-    ... ]
-    }
-    """
-=======
 '''
-def test_connection(self):
+    def test_connection(self):
     with app.app_context():
-        return
-'''
+    return
+    '''
 
 #data = json.load(open('link_analysis.json'))
 #@app.endpoint("/")
 #def test_json():
 #@app.route('/todo/api/v1.0/data', methods=['GET'])
 def getPageRank():
-     data = json.load(open('link_analysis.json'))
-     return data
- 
+    data = json.load(open('link_analysis.json'))
+        return data
+
 def getIndexing():
     data = json.load(open('indexing.json'))
     return data
 
 def getQuery():
     return
->>>>>>> master
 
 
 
@@ -58,8 +38,8 @@ def checkweight(urls, weights):
     for item in urls:
         print(weights[item])
 '''
-Iterate through indexing Json and create dictionary with all url keys
-'''
+    Iterate through indexing Json and create dictionary with all url keys
+    '''
 def create_dict(indexing):
     weights = {}
     #print(indexing["documents"][0]["documentID"])
@@ -70,33 +50,75 @@ def create_dict(indexing):
     return weights, all_urls
 
 '''
-Function adds weight to the dictionary of urls for each url
-'''
+    Function adds weight to the dictionary of urls for each url
+    '''
 def add_weight(indexing, link_analysis, urls, weights):
     ## Add link_analysis weight will update multiplayer when testing
     print(len(link_analysis["test"]))
     for item in link_analysis["test"]:
         add = item["pageRankValue"] * .2
-        print(add)
         weights[item["webpage"]] = add
-        
     ## Add indexing weight multiplier
+    for item in indexing["tokens"]:
+        token_size = item["ngramSize"]
+        for doc in item["documentOccurences"]:
+            count = len(doc["locations"])
+            add = count * token_size * .2
+            weights[doc["documentID"]] += add
+    return weights
+'''
+    Function creates a position dictionary that will be used for query json
+    '''
+def get_position(url, indexing):
+    position_dict = {}
+    for item in indexing["tokens"]:
+        n_gram = item["token"]
+        for doc in item["documentOccurences"]:
+            if doc["documentID"] == url:
+                locations = []
+                for loc in doc["locations"]:
+                    locations.append(loc)
+                position_dict[n_gram] = locations
+    return position_dict
+'''
+    Create json to hand off to querying
+    '''
+def create_query_json(sorted_keys, indexing):
+    query_dict = {}
+    rank = 1
+    url_info = []
+    for url in sorted_keys:
+        inside_dict = {}
+        inside_dict["url"] = url
+        inside_dict["rank"] = rank
+        position = get_position(url,indexing)
+        inside_dict["position"] = position
+        rank+=1
+        url_info.append(inside_dict)
     
-    
-    checkweight(urls,weights)
+    query_dict["ranking"] = url_info
+    json_string = json.dumps(query_dict)
+    return json_string
+
+
+
+'''
+    Function creates Json that will be handed off to Link Analysis
+    '''
+def create_link_json(urls):
+    link_json = {}
+    link_json["webpages"] = urls
+    json_string = json.dumps(link_json)
+    return json_string
+
 if __name__ == "__main__":
-    #test_connection(app)
-    #app.run(debug=True)
-    #data = getPageRank()
+    
     link_analysis = getPageRank()
     indexing = getIndexing()
     weights, urls = create_dict(indexing)
     weights = add_weight(indexing, link_analysis, urls, weights)
-    
-    
-    
-    
-    
-    
-    #pprint(data)
-    #pprint(indexing)
+    checkweight(urls,weights)
+    sorted_keys = sorted(weights, key=weights.get, reverse = True)
+    #print(create_link_json(urls))
+    print(create_query_json(sorted_keys, indexing))
+
