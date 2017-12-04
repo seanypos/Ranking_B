94ef74bd947cc906e426675a2978fb40c49cebc3
aleung013 <aleung013@gmail.com>
2017-12-03 23:05:03 -0500

    Change name of API endpoints

--- api.py
+++ api.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 import reilly.py
 
-@app.route('/query/<uuid>', methods=["POST"])
-def querying(uuid):
+@app.route('/ranking', methods=["POST"])
+def ranking(uuid):
     content = request.get_json(silent=True) 
     # TODO: Parse content to get query
     # TODO: Call indexing and link analysis APIs and then rank webpages
@@ -13,8 +13,8 @@ def querying(uuid):
     # TODO: data = ranked webpages
     return json.dumps(data)
 
-@app.route('/clicks', methods=["POST"])
-def clicks(uuid):
+@app.route('/stats', methods=["POST"])
+def stats(uuid):
     content = requests.get_json(silent=True)
     # TODO: update dictionary of clicks to use in weighting algorithm
     return uuid
